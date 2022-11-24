cc556453e37516fb43973aa6644dcb9aee5446af
Mikael Simberg <mikael.simberg@iki.fi>
2022-11-24 15:19:36 +0200

    Clean up includes and namespaces in dlaf integration

--- src/fm/cholesky_dla.cpp
+++ src/fm/cholesky_dla.cpp
@@ -9,59 +9,30 @@
 //
 
 #include <blas/util.hh>
-#include <omp.h>
-#include <mkl_service.h>
+#include <dlaf/communication/communicator.h>
+#include <dlaf/communication/communicator_grid.h>
+#include <dlaf/communication/error.h>
+#include <dlaf/factorization/cholesky.h>
+#include <dlaf/init.h>
+#include <dlaf/matrix/distribution.h>
+#include <dlaf/matrix/index.h>
+#include <dlaf/matrix/layout_info.h>
+#include <dlaf/matrix/matrix.h>
+#include <dlaf/matrix/matrix_mirror.h>
+#include <dlaf/types.h>
 #include <cstdlib>
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <mpi.h>
-#include <pika/future.hpp>
+#include <pika/execution.hpp>
 #include <pika/init.hpp>
 #include <pika/program_options.hpp>
 #include <pika/runtime.hpp>
-#include <pika/unwrap.hpp>
 
-#include "dlaf/matrix/matrix.h"
-
-#include "dlaf/auxiliary/norm.h"
-#include "dlaf/blas/tile.h"
-#include "dlaf/common/format_short.h"
-#include "dlaf/common/timer.h"
-#include "dlaf/communication/communicator_grid.h"
-#include "dlaf/communication/error.h"
-#include "dlaf/communication/init.h"
-#include "dlaf/communication/sync/broadcast.h"
-#include "dlaf/factorization/cholesky.h"
-#include "dlaf/init.h"
-#include "dlaf/matrix/copy.h"
-#include "dlaf/matrix/matrix.h"
-#include "dlaf/matrix/matrix_mirror.h"
-#include "dlaf/types.h"
-#include "dlaf/util_matrix.h"
-#include "dlaf/matrix/print_numpy.h"
-
-using pika::execution::experimental::keep_future;
-using pika::execution::experimental::start_detached;
-using pika::execution::experimental::when_all;
-
-using dlaf::Backend;
-using dlaf::Coord;
-using dlaf::DefaultDevice_v;
-using dlaf::Device;
-using dlaf::GlobalElementIndex;
-using dlaf::GlobalElementSize;
-using dlaf::GlobalTileIndex;
-using dlaf::LocalTileIndex;
-using dlaf::Matrix;
-using dlaf::SizeType;
-using dlaf::TileElementIndex;
-using dlaf::TileElementSize;
-using dlaf::comm::Communicator;
-using dlaf::comm::CommunicatorGrid;
-using dlaf::comm::Index2D;
-using dlaf::common::Ordering;
-using dlaf::internal::transformDetach;
-using dlaf::matrix::MatrixMirror;
+// TODO: Remove once https://github.com/eth-cscs/DLA-Future/pull/668 is
+// merged.
+#include <omp.h>
+#include <mkl_service.h>
 
 static bool dlaf_init_ = false;
 
@@ -107,7 +78,6 @@ extern "C" void dlaf_init() {
     int argc = 1;
     const char *argv[] = {"--pika:print-bind", /* tmp.c_str(),*/ nullptr};
     options_description desc_commandline("cp2k_dlaf");
-    //desc_commandline.add(dlaf::miniapp::getMiniappOptionsDescription());
     desc_commandline.add(dlaf::getOptionsDescription());
     
     /* pika initialization. Last parameter to be added. */
@@ -115,6 +85,8 @@ extern "C" void dlaf_init() {
     p.desc_cmdline = desc_commandline;
     p.rp_callback = dlaf::initResourcePartitionerHandler;
     pika::start(nullptr, argc, argv, p);
+
+    /* DLA-Future initialization */
     const int argc_dla = 1;
     const char *argv_dla[] = {"cp2k_dlaf", nullptr};
     dlaf::initialize(argc_dla, argv_dla);
@@ -167,9 +139,8 @@ template <typename T> void pxpotrf_dla(char uplo__, int n__, T *a__, int ia__, i
 
   single_threaded_omp sto{};
 
-  using dlaf::common::make_data;
-
   pika::resume();
+
   // matrix sizes
   int m, n;
 
@@ -190,25 +161,25 @@ template <typename T> void pxpotrf_dla(char uplo__, int n__, T *a__, int ia__, i
   //      - general cleanup
   // DONE - fortran interface uplo
   //      - cblcs call
+  //      - remove omp/mkl_set_num_threads calls (will be handled by DLAF
+  //        in https://github.com/eth-cscs/DLA-Future/pull/668)
 
   int np, mp, size;
   MPI_Comm comm = get_communicator(desca__[1]);
-  Communicator world(comm);
-  //std::cerr << "calling initial MPI_Barrier\n";
+  dlaf::comm::Communicator world(comm);
   DLAF_MPI_CHECK_ERROR(MPI_Barrier(world));
-  //std::cerr << "called initial MPI_Barrier\n";
   int dims[2] = {0, 0};
   int periods[2] = {0, 0};
   int coords[2] = {-1, -1};
   MPI_Comm_size(comm, &size);
   Cblacs_gridinfo(desca__[1], dims, dims + 1, coords, coords + 1);
 
-  CommunicatorGrid comm_grid(world, dims[0], dims[1], Ordering::RowMajor);
+  dlaf::comm::CommunicatorGrid comm_grid(world, dims[0], dims[1], dlaf::common::Ordering::RowMajor);
   
   // Allocate memory for the matrix
-  GlobalElementSize matrix_size(n, m);
-  TileElementSize block_size(nb, mb);
-  Index2D src_rank_index(0,0);
+  dlaf::GlobalElementSize matrix_size(n, m);
+  dlaf::TileElementSize block_size(nb, mb);
+  dlaf::comm::Index2D src_rank_index(0,0);
   dlaf::matrix::Distribution distribution(matrix_size,
                                           block_size,
                                           comm_grid.size(),
@@ -221,10 +192,10 @@ template <typename T> void pxpotrf_dla(char uplo__, int n__, T *a__, int ia__, i
   const int ld_ = desca__[8];
 
   dlaf::matrix::LayoutInfo layout = colMajorLayout(distribution, ld_);
-  Matrix<T, Device::CPU> mat(std::move(distribution), layout, a__);
+  dlaf::matrix::Matrix<T, dlaf::Device::CPU> mat(std::move(distribution), layout, a__);
 
   {
-    MatrixMirror<T,  dlaf::Device::Default, Device::CPU> matrix(mat);
+    dlaf::matrix::MatrixMirror<T,  dlaf::Device::Default, dlaf::Device::CPU> matrix(mat);
 
     switch(uplo__) {
      case 'U':
